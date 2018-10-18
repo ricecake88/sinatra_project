6688c5816a47b7727ed93ae56c318bed08eeb620
Grace S <vocalise@gmail.com>
2018-10-17 20:01:07 -0700

    Modified category files

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -8,4 +8,16 @@ class CategoryController < ApplicationController
     get '/categories' do
         erb :'category/index'
     end
+
+    post '/categories' do
+        name = params[:category_name]
+        cat = Category.create(:category_name => name)
+        Category.all << cat
+        cat.save
+
+        redirect to '/categories'
+    end
+
+    helpers do 
+    end
 end
