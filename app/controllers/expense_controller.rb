f8da983ec67db52928fbef1c3e197b557d72a585
Grace Shih <vocalise@gmail.com>
2018-10-25 04:03:21 +0000

    Modified add expsense controller and views and added show

--- app/controllers/expense_controller.rb
+++ app/controllers/expense_controller.rb
@@ -1,15 +1,54 @@
+require 'rack-flash'
+
 class ExpenseController < ApplicationController
+  use Rack::Flash
+  enable :sessions
 
-    get '/expense' do
-      @expenses = Expense.all
-      erb :'expense/index'
-    end
+  get '/expense' do
+    @expenses = Expense.all
+    erb :'expense/index'
+  end
 
-    get '/expense/add' do
-      erb :'expense/add'
-    end
+  get '/expense/add' do
+    erb :'expense/add'
+  end
 
-    post '/expense/add' do
-      binding.pry
+  get '/expense/:id' do
+    @expense = Expense.find(params[:id])
+    erb :'expense/show'
+  end
+
+  post '/expense/add' do
+    if (!params[:expense]["date"].empty? &&
+        !params[:expense]["amount"].empty? &&
+        !params[:expense]["description"].empty? &&
+        !params[:expense]["vendor"].empty?)
+        @matched_expense = entry_already_exists?(params[:expense])
+        if !@matched_expense
+          @expense = Expense.create(params[:expense])
+          Expense.all << @expense
+          @expense.save
+          flash[:message] = "Expense added"
+          redirect to "/expense/#{@expense.id}"
+        else
+          flash[:message] = "Already added"
+          redirect '/expense/add'
+        end
+    else
+      flash[:message] = "Missing Fields"
+      redirect '/expense/add'
     end
+  end
+
+  helpers do
+    def entry_already_exists?(expense)
+      @matched_expense_by_date = Expense.find_by(:date => expense['date'])
+      if (@matched_expense_by_date["vendor"] == expense["vendor"] &&
+          @matched_expense_by_date["amount"].to_f == expense["amount"].to_f)
+          return true
+      else
+        return false
+      end
+    end
+  end
 end
