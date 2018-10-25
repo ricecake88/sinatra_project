eb315bc5759a19cea086413faffd8e5ac84f76d9
Grace Shih <vocalise@gmail.com>
2018-10-25 04:02:46 +0000

    Modified migration for expenses

--- db/migrate/20181011041448_create_expenses.rb
+++ db/migrate/20181011041448_create_expenses.rb
@@ -1,10 +1,11 @@
 class CreateExpenses < ActiveRecord::Migration
   def change
     create_table :expenses do |t|
-      t.date :expense_date
-      t.decimal :expense_amount, :precision => 8, :scale => 2
-      t.string :expense_description
-      t.integer :expense_category_id
+      t.date :date
+      t.decimal :amount, :precision => 8, :scale => 2
+      t.string :description
+      t.integer :category_id
+      t.string :vendor
     end
   end
 end
