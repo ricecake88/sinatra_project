b7652028ab6355645ab2a99400b610383ad9386a
Grace Shih <vocalise@gmail.com>
2018-10-30 04:34:11 +0000

    Preliminary code added to support users and sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,28 @@ class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+  enable :sessions
 
   get '/' do
     erb :'index'
   end
 
+  post '/login' do
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      erb :error
+    end
+  end
+
+  get '/account' do
+    @sessionName = session
+  end
+
+  get '/logout' do
+    session.clear
+    redirect to '/'
+  end
 end
