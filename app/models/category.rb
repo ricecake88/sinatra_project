67907ca0bdda6cbbf2ae6f2ac4b21843a625226b
Grace Shih <vocalise@gmail.com>
2018-11-16 08:11:53 +0000

    Added support for user and for adding category when adding edding expense

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,9 @@
 class Category < ActiveRecord::Base
-  def self.create_category_if_empty
+  def self.create_category_if_empty(sessionName)
+    binding.pry
+    user = Helpers.current_user(sessionName)
     if !Category.any?
-      cat = Category.new(:category_name => "Expenses")
+      cat = Category.new(:category_name => "Expenses", :user_id => user.id)
       Category.all << cat
       cat.save
     end
