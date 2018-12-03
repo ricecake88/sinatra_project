5ed7c6d52721ef7719f58665aad0e8926a189cec
Grace Shih <vocalise@gmail.com>
2018-12-03 04:58:04 +0000

    Removed Expense_Users table, controller, migration and databases

--- app/models/expenses_users.rb
+++
@@ -1,4 +0,0 @@
-class Expenses_Users < ActiveRecord::Base
-    belongs_to :expenses
-    belongs_to :users
-end
--- db/migrate/20181111001126_create_budget_expenses.rb
+++
@@ -1,8 +0,0 @@
-class CreateBudgetExpenses < ActiveRecord::Migration
-  def change
-    create_table :expenses_users do |t|
-      t.integer :expense_id
-      t.integer :user_id
-    end
-  end
-end
