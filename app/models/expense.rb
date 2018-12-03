a966f1cb03ae3b2d4fa5b67b668f9f0402ef7fd5
Grace Shih <vocalise@gmail.com>
2018-12-03 02:00:19 +0000

    Added function to computer only expenses related to user that is logged in

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,3 +1,10 @@
 class Expense < ActiveRecord::Base
     has_many :categories
+
+    def self.total_expenses_for_user(expenses)
+      total = 0
+      expenses.each do
+        total+=expense.amount
+      end
+    end
 end
