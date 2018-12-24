300b10d4ce16d9fac0d72fd5d1b03afe95fbb7c8
Grace Shih <vocalise@gmail.com>
2018-12-24 20:20:10 +0000

    Added two methods to get current month and current year

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -11,4 +11,14 @@ class Helpers
     end
   end
 
+  def self.current_month()
+    d = DateTime.now
+    d.strftime("%m").to_i
+  end
+
+  def self.current_year()
+    d = DateTime.now
+    d.strftime("%Y").to_i
+  end
+
 end
