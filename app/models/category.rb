90ded8f6ba18e28c9fabcdcfb166e233d044dd6c
Grace Shih <vocalise@gmail.com>
2018-12-30 03:28:17 +0000

    Create a new Expense category if user's categories are emptyu

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
   def self.create_category_if_empty(sessionName)
     user = Helpers.current_user(sessionName)
-    if !Category.any?
+    if Category.find_by(:user_id => user.id).nil?
       cat = Category.new(:category_name => "Expenses", :user_id => user.id)
       Category.all << cat
       cat.save
