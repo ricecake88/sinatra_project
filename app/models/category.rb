efa45c1e989a439d34271ca1c8828ab5c026c069
Grace Shih <vocalise@gmail.com>
2019-01-05 06:39:19 +0000

    Added method that would sort categories by name

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,8 +12,8 @@ class Category < ActiveRecord::Base
     end
   end
 
-  def self.categories_of_user(sessionName)
-    user = Helpers.current_user(sessionName)
-    categories = Category.where(:user_id => user.id)
+  def self.sort_categories(sessionName)
+    categories = Helpers.current_user(sessionName).categories
+    categories.sort_by &:category_name
   end
 end
