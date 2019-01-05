d8fee90f03a1869eff761363108bfdb0b12808cb
Grace Shih <vocalise@gmail.com>
2019-01-05 05:24:43 +0000

    Updated User Model to remove the recursive categories association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ActiveRecord::Base
   has_many :user_budgets
   has_many :categories
   has_many :expenses, through: :user_expenses
-  has_many :categories, through: :categories
   has_many :budgets, through: :user_budgets
 end
