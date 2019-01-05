19e43cfdcdec5eb6a32aca234a7c448613eb9b17
Grace Shih <vocalise@gmail.com>
2019-01-05 08:14:56 +0000

    Removed unneeded methods due to redesign with join tables

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,27 +2,6 @@ class Expense < ActiveRecord::Base
     has_many :user_expenses
     belongs_to :user
 
-    def self.total_expenses_for_user(expenses)
-      total = 0
-      expenses.each do |expense|
-        total+=expense.amount
-      end
-      total
-    end
-
-    def self.expenses_for_user(sessionName)
-      @expenses = []
-      categories_user = Category.categories_of_user(sessionName)
-      if !categories_user.nil?
-        Expense.all.order(date: :asc).each do |expense|
-          if categories_user.ids.include?(expense.category_id)
-            @expenses << expense
-          end
-        end
-      end
-      @expenses
-    end
-
     def self.expenses_by_user_category(sessionName, category_id)
       @expenses = Expense.where(:category_id => category_id).order(date: :desc)
     end
