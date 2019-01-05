ef283d9b0120a6d382155b3f8425dc2ceacbc31d
Grace Shih <vocalise@gmail.com>
2019-01-05 05:22:25 +0000

    Associated user categories with default category and default category user with current user

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,9 @@ class Category < ActiveRecord::Base
   def self.create_category_if_empty(sessionName)
     user = Helpers.current_user(sessionName)
     if Category.find_by(:user_id => user.id).nil?
-      cat = Category.new(:category_name => "Expenses", :user_id => user.id)
+      cat = Category.new(:category_name => "Expenses")
+      cat.user = user
+      user.categories << cat
       Category.all << cat
       cat.save
     end
