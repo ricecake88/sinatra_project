e00a27524c90b1e3057d5cedff1a5e3eae14024f
Grace Shih <vocalise@gmail.com>
2019-01-11 21:10:22 +0000

    Added validates_presence_of username and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   has_many :categories
   has_many :expenses, through: :user_expenses
   has_many :budgets, through: :user_budgets
+  validates_presence_of :username, :password
 end
