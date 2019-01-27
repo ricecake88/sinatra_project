e0b1c149f38ceff9b26c7ece11b7defa54616df5
Grace Shih <vocalise@gmail.com>
2019-01-27 03:46:32 +0000

    Updated category controller and refactored valid methods

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -3,120 +3,91 @@ class CategoryController < ApplicationController
 
   get '/categories' do
     @categories = []
-    user = current_user
-    if is_logged_in? && !user.nil?
-      Category.create_category_if_empty(session)
-      @categories = user.categories_sorted
-      erb :'categories/index', :layout => :layout_loggedin
-    else
-      flash[:message] = "Illegal action. Please log-in to access this page."
-      redirect '/'
-    end
+    redirect_if_not_logged_in
+    Category.create_category_if_empty(session)
+    @categories = current_user.categories_sorted
+    erb :'categories/index', :layout => :layout_loggedin
   end
 
   patch '/categories/edit' do
-    user = current_user
-    if is_logged_in? && !user.nil?
-      if !params[:category].nil?
-        @categories = params[:category]
-        @categories.each do |cat|
-          category = Category.find(cat["id"])
-          if !category.nil? && user == category.user
-            if cat["name"] != category.category_name
-              category.update(:category_name => cat["name"])
-              category.save
-              flash[:message] = "Modified category"
-            end
-          else
-            flash[:message] = "You do not have permission to do that."
-            redirect to '/', :layout => :layout_loggedin
+    redirect_if_not_logged_in
+    if !params[:category].nil?
+      @categories = params[:category]
+      @categories.each do |cat|
+        category = Category.find(cat["id"])
+        if !category.nil? && current_user == category.user
+          if cat["name"] != category.category_name
+            category.update(:category_name => cat["name"])
+            category.save
+            flash[:message] = "Modified category"
           end
+        else
+          flash[:message] = "You do not have permission to do that."
+          redirect to '/', :layout => :layout_loggedin
         end
-      else
-        flash[:message] = "No category modified, missing category data."
       end
     else
-      flash[:message] = "Illegal action. Please log-in to access this page."
-      redirect '/'
+      flash[:message] = "No category modified, missing category data."
     end
     redirect to '/categories'
   end
 
   post '/categories/new' do
-    user = current_user
-    if is_logged_in? && !user.nil?
-      if !params[:category_name].empty?
-        if !exists_already?(params[:category_name])
-          name = params[:category_name]
-          category = Category.new(:category_name => params[:category_name])
-          category.user = user
-          if category.save
-            user.categories << category
-            Category.all << category
-            flash[:message] = "Added category!"
-          end
-        else
-          flash[:message] = "Error, category already exists"
+    redirect_if_not_logged_in
+    if !params[:category_name].empty?
+      if !exists_already?(params[:category_name])
+        name = params[:category_name]
+        category = Category.new(:category_name => params[:category_name])
+        category.user = current_user
+        if category.save
+          current_user.categories << category
+          Category.all << category
+          flash[:message] = "Added category!"
         end
       else
-        flash[:message] = "Error, category is empty."
+        flash[:message] = "Error, category already exists"
       end
-      redirect to '/categories'
     else
-      flash[:message] = "Illegal action. Please log-in to access this page."
-      redirect to '/'
+      flash[:message] = "Error, category is empty."
     end
+    redirect to '/categories'
   end
 
   get '/categories/delete' do
-    user = current_user
-    if is_logged_in? && !user.nil?
-      @categories = Category.sort_categories(session)
-      erb :'/categories/delete', :layout => :layout_loggedin
-    else
-      flash[:message] = "Illegal action. Please log-in to access this page."
-      redirect '/'
-    end
+    redirect_if_not_logged_in
+    @categories = Category.sort_categories(session)
+    erb :'/categories/delete', :layout => :layout_loggedin
   end
 
   delete '/categories/delete' do
-    user = current_user
-    if is_logged_in?  && !user.nil?
-      @categories = params[:category]
-      if !@categories.nil?
-        @categories.each do |cat|
-          expenses_current_category = Expense.where(:category_id => cat["id"])
-          if !expenses_current_category.empty?
-            set_category_to_default(expenses_current_category)
-          end
-          category = Category.find(cat["id"])
-          if !category.nil? && user == category.user
-            category.delete
-            flash[:message] = "Category Deleted"
-            redirect to '/categories'
-          else
-            flash[:message] = "You do not have permission to do that."
-            redirect to '/', :layout => :layout_loggedin
-          end
+    redirect_if_not_logged_in
+    @categories = params[:category]
+    if !@categories.nil?
+      @categories.each do |cat|
+        expenses_current_category = Expense.where(:category_id => cat["id"])
+        if !expenses_current_category.empty?
+          set_category_to_default(expenses_current_category)
         end
-      else
-        flash[:message] = "No categories selected."
-        redirect to '/categories'
+        category = Category.find(cat["id"])
+        if !category.nil? && user == category.user
+          category.delete
+          flash[:message] = "Category Deleted"
+          redirect to '/categories'
+        else
+          flash[:message] = "You do not have permission to do that."
+          redirect to '/', :layout => :layout_loggedin
+        end
       end
     else
-      flash[:message] = "Illegal action. Please log-in to access this page."
-      redirect '/'
+      flash[:message] = "No categories selected."
+      redirect to '/categories'
     end
   end
 
   helpers do
 
     def exists_already?(name)
-      name = Category.find_by(:category_name => name, :user_id => session[:user_id])
-      if !name.nil?
-        return true
-      end
-      return false
+      !Category.find_by(:category_name => name, :user_id => session[:user_id]).nil?
     end
 
     def set_category_to_default(expenses)
