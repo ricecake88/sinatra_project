552fc4b245edcfb10a28b31063e26332700a3ee3
Grace Shih <vocalise@gmail.com>
2019-01-27 01:57:51 +0000

    Preliminary cleanup of budget controller

--- app/controllers/budget_controller.rb
+++ app/controllers/budget_controller.rb
@@ -1,57 +1,35 @@
 class BudgetController < ApplicationController
 
   get '/budgets' do
-    user = current_user
-    if is_logged_in? && !user.nil?
-      @budgets = user.budgets
-      @categories = user.categories
+    redirect_if_not_logged_in
+      @budgets = current_user.budgets
+      @categories = current_user.categories
       erb :'budgets/index', :layout => :layout_loggedin
-    else
-      flash[:message] = "Illegal action. Please log-in to access this page."
-      redirect '/'
-    end
-
   end
 
   get '/budgets/new' do
-    user = current_user
-    if is_logged_in? && !user.nil?
+    redirect_if_not_logged_in
       Category.create_category_if_empty(session)
-      @categories = user.categories_sorted
+      @categories = current_user.categories_sorted
       erb :'/budgets/new', :layout => :layout_loggedin
-    else
-      flash[:message] = "Illegal action. Please log-in to access this page."
-      redirect '/'
-    end
   end
 
   get '/budgets/:id/edit' do
-    user = current_user
-    if is_logged_in? && !user.nil?
+    redirect_if_not_logged_in
       @budget = Budget.find(params[:id])
-      @categories = user.categories_sorted
+      @categories = current_user.categories_sorted
       erb :'/budgets/edit', :layout => :layout_loggedin
-    else
-      flash[:message] = "Illegal action. Please log-in to access this page."
-      redirect '/'
-    end
   end
 
   get '/budgets/:id' do
-    user = current_user
-    if is_logged_in? && !user.nil?
+    redirect_if_not_logged_in
       @budget = Budget.find(params[:id])
       @categories = Helpers.current_user(session).categories
       erb :'/budgets/show', :layout => :layout_loggedin
-    else
-      flash[:message] = "Illegal action. Please log-in to access this page."
-      redirect '/'
-    end
   end
 
   post '/budgets/create' do
-    user = current_user
-    if is_logged_in? && !user.nil?
+    redirect_if_not_logged_in
       if cat_exists?(params[:budget]['category'])
         flash[:message] = "OOPS, already set a budget for this category. "
         redirect to "/budgets"
@@ -61,7 +39,7 @@ class BudgetController < ApplicationController
       elsif !params[:budget]["amount"].empty? && !params[:budget]["category"].empty?
         @budget = Budget.new(:category_id => params[:budget]["category"].to_i, :amount => params[:budget]["amount"], :rollover => params[:budget]["rollover"])
         @category = Category.find(params[:budget][:category].to_i)
-        if user == Category.find(@budget.category_id).user && @budget.save
+        if current_user == Category.find(@budget.category_id).user && @budget.save
           @category.budget = @budget
           Budget.all << @budget
           redirect to "/budgets/#{@budget.id}"
@@ -73,34 +51,24 @@ class BudgetController < ApplicationController
         flash[:message] = "Sorry, either the amount or category entered is empty"
         redirect to "/budgets/add"
       end
-    else
-      flash[:message] = "Illegal action. Please log-in to access this page."
-      erb :'/'
-    end
   end
 
 patch '/budgets/:id/edit' do
-  user = current_user
-  if is_logged_in? && !user.nil?
+  redirect_if_not_logged_in
       @budget = Budget.find(params[:id])
       @budget.update(:amount => params[:amount], :rollover => params[:rollover])
-      if user == Category.find(@budget.category_id).user && @budget.save
+      if current_user == Category.find(@budget.category_id).user && @budget.save
         redirect to '/budgets'
       else
         flash[:message] = "You do not have permission to do that."
         redirect to '/'
       end
-  else
-    flash[:message] = "Illegal action. Please log-in to access this page."
-    redirect '/'
-  end
 end
 
 delete '/budgets/:id/delete' do
-  user = current_user
-  if is_logged_in? && !user.nil?
+  redirect_if_not_logged_in
     @budget = Budget.find(params[:id])
-    if @budget && user == Category.find(@budget.category_id).user
+    if @budget && current_user == Category.find(@budget.category_id).user
       @budget.delete
       flash[:message] = "Budget Deleted"
       redirect to '/budgets'
@@ -108,10 +76,6 @@ delete '/budgets/:id/delete' do
       flash[:message] = "You do not have permission to do that."
       redirect to '/'
     end
-  else
-    flash[:message] = "Illegal action. Please log-in to access this page."
-    redirect to '/'
-  end
 end
 
   helpers do
