0c17596b746f2e54eb7d0e1733dbe104bd3ce19e
Grace Shih <vocalise@gmail.com>
2019-01-29 04:28:42 +0000

    Removed methods in helpers that are already in application_controller

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,16 +1,4 @@
 class Helpers
-  def self.current_user(sessName)
-      @user = User.find(sessName[:user_id])
-  end
-
-  def self.is_logged_in?(sessName)
-    if sessName[:user_id].nil?
-      false
-    else
-      true
-    end
-  end
-
   def self.current_month()
     d = DateTime.now
     d.strftime("%m").to_i
