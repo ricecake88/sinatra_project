4ba90a2e900a5eb1c02e4f8ce66381b5cadb0863
Grace Shih <vocalise@gmail.com>
2019-02-14 06:28:00 +0000

    Updated methods needed to clean up post and delete actions for categories

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -32,24 +32,12 @@ class CategoryController < ApplicationController
   post '/categories/new' do
     redirect_if_not_logged_in
     redirect_if_category_is_invalid(params[:category_name])
-    #if !params[:category_name].empty?
-      #if !exists_already?(params[:category_name])
-        #name = params[:category_name]
-        category = Category.new(:category_name => params[:category_name])
-        category.user = current_user
-        if category.save
-          #current_user.categories << category
-          #Category.all << category
-          flash[:message] = "Added category!"
-          redirect to '/categories'
-        end
-      #else
-      #  flash[:message] = "Error, category already exists"
-      #end
-    #else
-    #  flash[:message] = "Error, category is empty."
-    #end
-    #redirect to '/categories'
+    category = Category.new(:category_name => params[:category_name])
+    category.user = current_user
+    if category.save
+      flash[:message] = "Added category!"
+      redirect to '/categories'
+    end
   end
 
   get '/categories/delete' do
@@ -60,30 +48,15 @@ class CategoryController < ApplicationController
 
   delete '/categories/delete' do
     redirect_if_not_logged_in
-    redirect_if_categories_invalid(params[:category])
-  #  @categories = params[:category]
-  #  if @categories
-      params[:category].each do |cat|
-        category = Category.find_by(:id => cat["id"])
-        binding.pry
-        redirect_if_not_valid_user_or_record(category)
-        redirect_if_invalid_category(category)
-        #redirect_if_expense_category(cat["id"])
-        set_category_to_default2(category)
-        binding.pry
-        #expenses_in_current_category = Expense.where(:category_id => cat["id"])
-        #if !expenses_in_current_category.empty?
-        #  set_category_to_default(expenses_in_current_category)
-        #end
-
-        #category = Category.find_by(:id => cat["id"])
-
-        category.delete
-        flash[:message] = "Category or Categories Deleted."
-      end
-    #else
-    #  flash[:message] = "No categories selected."
-    #end
+    redirect_if_no_categories(params[:category])
+    params[:category].each do |cat|
+      category = Category.find_by(:id => cat["id"])
+      redirect_if_not_valid_user_or_record(category)
+      redirect_if_category_is_invalid(category.category_name)
+      set_category_to_default(category)
+      category.delete
+      flash[:message] = "Category or Categories Deleted."
+    end
     redirect to '/categories'
   end
 
@@ -93,37 +66,20 @@ class CategoryController < ApplicationController
       !Category.find_by(:category_name => name, :user_id => session[:user_id]).nil?
     end
 
-    def set_category_to_default(expenses)
-      binding.pry
-      default_user_category_id = 0
-
-      current_user.categories.each do |cat|
-        if cat.category_name == "Expenses"
-          default_user_category_id = cat.id
-          break
-        end
-      end
-
-      expenses.each do |expense|
-        expense_row = Expense.find(expense.id)
-        expense_row.update(:category_id => default_user_category_id)
-      end
-    end
-
-    def set_category_to_default2(category)
+    def set_category_to_default(category)
       if !category.expenses.empty?
         default_user_category_id = 0
 
         current_user.categories.each do |cat|
           if cat.category_name == "Expenses"
-            default_user_category_id = category.id
+            default_user_category_id = cat.id
             break
           end
         end
 
-        expenses.each do |expense|
-          expense_row = Expense.find(expense.id)
-          expense_row.update(:category_id => default_user_category_id)
+        category.expenses.each do |ex|
+          ex.category_id = default_user_category_id
+          ex.update(:category_id => default_user_category_id)
         end
       end
     end
@@ -135,25 +91,13 @@ class CategoryController < ApplicationController
       end
     end
 
-    def redirect_if_invalid_category(category)
-      valid = false
-      path = '/categories'
-      if category.category_name == "Expenses"
-        flash[:message] = "You do not have permission to do that."
-        path = '/'
-      elsif category.category_name.empty?
-        flash[:message] = "Error, category is empty."
-      else
-        valid = true
-      end
-      if !valid
-        redirect to path
-      end
-    end
-
     def redirect_if_category_is_invalid(name)
       valid = false
-      if name.empty?
+      path = '/categories'
+      if name == "Expenses"
+        flash[:message] = "You do not have permission to do that."
+        path = '/'
+      elsif name.empty?
         flash[:message] = "Error, category is empty."
       elsif exists_already?(name)
         flash[:message] = "Error, category already exists"
@@ -165,14 +109,9 @@ class CategoryController < ApplicationController
       end
     end
 
-    def redirect_if_categories_invalid(category_ids)
-      valid = false
+    def redirect_if_no_categories(category_ids)
       if !category_ids
         flash[:message] = "No categories selected"
-      else
-        valid = true
-      end
-      if !valid
         redirect to '/categories'
       end
     end
